9e2c8143d505f71a6761708b037000887f00bf3a
Luca <s0narr@protonmail.com>
2025-05-12 22:08:02 +0300

    Adding header rotation for safety.

--- app/Utils/read_parquet.py
+++ app/Utils/read_parquet.py
@@ -1,8 +1,19 @@
 import pandas as pd
-
+import socket
 
 def get_links(path):
     domains = pd.read_parquet(path)["domain"].dropna().tolist()
-    links = ["http://" + domain for domain in domains]
+    # links = ["http://" + domain for domain in domains]
+    links = []
+
+    for domain in domains:
+        try:
+            resolved_ip = socket.gethostbyname(domain)
+            links.append({"domain": domain, "resolved_ip": resolved_ip})
+        except socket.gaierror:
+            print("Error resolving DNS.")
+            continue
+        
     return links
+
 
--- app/main.py
+++ app/main.py
@@ -1,30 +1,34 @@
 from Utils.read_parquet import get_links
 from Utils.scrape_html import scrape_html
-
 from Scripts import main_script
 
+import os
 import asyncio
 import httpx
 import time
 
 """ Global declarations. """
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PARQUET_PATH = os.path.join(BASE_DIR, "Data/logos.snappy.parquet")
+
+
 links = []
 results = []
 
-# async def main():
-    # start_time = time.time()
 
-    # links = get_links("data/logos.snappy.parquet")
+async def main():
+    start_time = time.time()
 
+    links = get_links(PARQUET_PATH)
 
-    # print("Read links from .parquet.")
-    # result = await scrape_html(links)
-    # print("---%s seconds---" % (time.time() - start_time))
+    print("Read links from .parquet.")
+    # result = await scrape_html(links[:1000])
+    print("---%s seconds---" % (time.time() - start_time))
 
     # print(result)
 
 
 if __name__ == '__main__':
-    asyncio.run(main_script())
+    asyncio.run(main())
 
