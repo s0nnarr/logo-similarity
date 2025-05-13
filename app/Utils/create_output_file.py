814850299c6442c33d6296ecf4d62c270eaee365
Luca <s0narr@protonmail.com>
2025-05-13 15:59:11 +0300

    Refresh

--- app/Utils/create_output_file.py
+++ app/Utils/create_output_file.py
@@ -3,6 +3,6 @@ from typing import Dict, List, Any
 
 def create_output(resolved_links: List[Dict[str, Any]], output_path="resolved_links.txt"):
 
-    with open(output_path, "a") as f:
+    with open(output_path, "w") as f:
         for link in resolved_links:
             f.write(f"{link}\n")
